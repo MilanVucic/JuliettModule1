44d69b2972b2b336f03472d5555cc2f761b14b1c
vucic <vucic94@yahoo.com>
2025-01-09 19:24:20 +0100

    Lesson 18

---
+++ src/hw/MyQueue.java
@@ -0,0 +1,12 @@
+package hw;
+
+public class MyQueue<T> {
+    // John10 Milan5 Michael5 Lisa5 Richard4
+    public void add(T elem) {
+
+    }
+
+    public T get() {
+        return null;
+    }
+}
